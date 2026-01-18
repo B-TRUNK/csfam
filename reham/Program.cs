faee6f13aeec6c01356ce79939ea7fcc8ff1b075
B-TRUNK <abanoub.boshra.anis@gmail.com>
2026-01-18 21:00:36 +0200

    vars

---
+++ reham/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Configuration.Assemblies;
+using System.Security.Cryptography;
+
+Console.WriteLine("Hello, Reham!");
+string name = "Reham Ragaye";
+char first_character ='R';
+int age = 32;
+float height = 1.56f ;
+float weight = 75f ;
+string country ="Egypt";
+
+
+Console.WriteLine(
+    "Your name is: " + name + "\n" +
+    "your first character:" + first_character + "\n" +
+    "your age is :" + age +"\n"+
+    "Your highet :"+ height + " "+ "meter " +"\n"+
+    "Your weight:" + weight + " " + "kilogeram " +"\n"+
+    "Your country : " + country + "\n"
+);
