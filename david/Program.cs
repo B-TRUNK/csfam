5e6b801863c665bcf3f808a7ffe8407354b6bf10
B-TRUNK <abanoub.boshra.anis@gmail.com>
2026-01-18 19:54:21 +0200

    vars

---
+++ david/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, David!");
+
+string name = "david abanoub anis";
+char first_character = 'd';
+int age  = 9;
+float height = 1.35f;
+float weight = 25f;
+string school_grade = "3rd_Primary";
+
+Console.WriteLine(
+    "Your Name is :" + name + "\n" +
+    "your first character:" + first_character + "\n" +
+    "Your Age is :" + age + " Years Old"
+    "your height is: "+height
+    );
